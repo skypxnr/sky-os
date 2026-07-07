d03a15a56c9035986fcd3f2f9c4d028c74077590
Sheikh Iyad <sheikhiyad60@gmail.com>
2026-07-07 23:11:17 +0530

    reading user input

--- boot.asm
+++ boot.asm
@@ -1,23 +1,42 @@
-[org 0x7c00]                  
+[org 0x7C00]
 
 start:
-    mov ah, 0x0e               
-    mov bx, variableName     
+    mov bx, string
+    mov cx, 0
+
+read_loop:
+    mov ah, 0
+    int 0x16
+    cmp al, 0x0D
+    je printString
+
+    cmp cx, 10
+    je printString
+
+    mov [bx], al
+    inc bx
+    inc cx
+    mov ah, 0x0E
+    int 0x10
+    jmp read_loop
 
 printString:
-    mov al, [bx]             
-    cmp al, 0                  
-    je end                      
-    int 0x10                   
-    inc bx                      
-    jmp printString              
+    mov byte [bx], 0
+    mov bx, string
+print_loop:
+    mov al, [bx]
+    cmp al, 0
+    je done
+    mov ah, 0x0E
+    int 0x10
+    inc bx
+    jmp print_loop
 
-end:
-    jmp $                         
+done:
+    jmp $
 
-variableName:
-    db "This is a string.", 0    
+string:
+    times 11 db 0
 
-times 510-($-$$) db 0          
-
-db 0x55, 0xaa 
+times 510 - ($ - $$) db 0
+db 0x55, 0xaa
